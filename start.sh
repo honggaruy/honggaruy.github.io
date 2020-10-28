12d35eab92d7bb81b6dfe97a7ca512a74afbeb38
honggaruy <honggaruy@gmail.com>
2020-10-28 19:22:21 +0900

    Jekyll 버전을 3에서 4로 올려라
    jekyll 홈페이지 공식 가이드 : https://jekyllrb-ko.github.io/docs/upgrading/3-to-4/
    johngrib님 관련 commit : https://github.com/johngrib/johngrib.github.io/commit/618582b04e3ac16f82bcc9fc2eea070dc5ff8aa7#diff-65a5db1ae8f8e01ebdbbfafcf4e24618ad7cffe036bba92dce1390383e5c73e5

--- start.sh
+++ start.sh
@@ -1,5 +1,9 @@
 bundle update && bundle install
 ./generateData.js
 
-# bundle exec jekyll server --watch
-docker-compose up
+# johngrib 수정사항 반영 commit id - afbc84c3bdb14b4d7f478f4f6f69b527b1ce4a8e
+if [[ $1 = "docker" ]]; then
+	docker-compose up
+elif [[ $1 = "watch" ]]; then
+	bundle exec jekyll server --incremental --trace
+fi
